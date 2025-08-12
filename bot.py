9337fd5de62a4a6abc7e9f86765734455a90c838
Aashu Yadav <kdsapm@gmail.com>
2025-08-12 05:49:55 +0000

    gjkn

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 from PIL import Image
 from pyzbar.pyzbar import decode
-
 import os
 
 # Replace with your bot's token
